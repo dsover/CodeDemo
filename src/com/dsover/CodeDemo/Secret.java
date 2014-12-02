95eb6712dae7d8c1daf6cffcdb2cdc923ede5d8b
Alex Sover <alexsover@gmail.com>
2014-12-02 10:40:09 -0500

    comments added to the Secret Class

--- src/com/dsover/CodeDemo/Secret.java
+++ src/com/dsover/CodeDemo/Secret.java
@@ -7,6 +7,13 @@ package com.dsover.CodeDemo;
 public class Secret {
 	public Secret(){}
 	
+	
+	/**
+	 * this function takes some integer and returns some integer
+	 * this is just a stub to demo the driver class
+	 * @param incomingInt
+	 * @return some integer from the function
+	 */
 	public int secretFunction(int incomingInt){
 		return 0;
 	}
