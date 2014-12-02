e4115ac3cbd82f0a1c48f4f3073a3af02eb96ed2
Alex Sover <alexsover@gmail.com>
2014-12-02 10:32:23 -0500

    this is the inital commit for the the 'Secret' class

---
+++ src/com/dsover/CodeDemo/Secret.java
@@ -0,0 +1,15 @@
+package com.dsover.CodeDemo;
+/**
+ * this is a real simple class to show the encapsulation of the 
+ * @author alex
+ *
+ */
+public class Secret {
+	public Secret(){}
+	
+	public int secretFunction(int incomingInt){
+		return 0;
+	}
+	
+	
+}
